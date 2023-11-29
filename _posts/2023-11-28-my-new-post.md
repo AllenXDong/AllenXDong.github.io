c3ad9ba335f6fc73ee2f7de898fbf3501c275ed6
316739861@qq.com <316739861@qq.com>
2023-11-29 09:51:36 +0800

    update

--- _posts/2023-11-28-my-new-post.md
+++
@@ -1,5 +0,0 @@
----
-layout: post
-title: My New Post
-date: 2023-11-28 18:24 +0800
----
--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1701221967';
+const cacheName = 'chirpy-1701222653';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
